3bb54da0933af1bb0f1df10bbbe87e125880a4f2
Ruby <rubyisenough@gmail.com>
2026-08-08 17:26:50 -0500

    changed torp

--- explosion.gd
+++ explosion.gd
@@ -17,17 +17,19 @@ func _ready() -> void:
 		if i is RigidBody3D:
 			var dir = global_position.direction_to(i.global_position)
 			i.apply_central_impulse(dir*ExplosionForce)
+	$Implosion.emitting = true
+	$Bubbles.emitting = true
 	var tween = get_tree().create_tween()
 	tween.set_parallel(true)
 	tween.set_trans(Tween.TRANS_QUAD)
 	tween.tween_property(shine, "omni_range", 13, 0.3)
-	tween.tween_property(shine, "light_energy", 9.5, 0.3)
+	tween.tween_property(shine, "light_energy", 9.5, 0.25)
 	tween.tween_property(collider, "scale", Vector3.ONE*4.5, 0.3)
 	await get_tree().create_timer(.35).timeout
 	tween = get_tree().create_tween()
 	tween.set_parallel(true)
 	tween.set_trans(Tween.TRANS_QUAD)
-	tween.tween_property(shine, "omni_range", 0, 0.3)
+	tween.tween_property(shine, "omni_range", 0, 0.25)
 	tween.tween_property(collider, "scale", Vector3.ZERO, 0.3)
-	await get_tree().create_timer(1).timeout
+	await get_tree().create_timer(5).timeout
 	queue_free()
